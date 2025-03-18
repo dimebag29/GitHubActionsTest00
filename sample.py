15722d2124d8f771246309eb6e43ea4f4e5f1495
dimebag29 <fused.deposition.modeling@gmail.com>
2025-03-18 17:09:28 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,14 +1,22 @@
+"""
 import numpy as np
 
-"""
 a = np.array([1, 2])
 b = np.array([3, 4])
 print(a + b)
 """
 
+"""
 with open('example.txt', 'w', encoding='utf-8') as file:
     # ファイルに書き込む内容
     file.write('こんにちは！これはテキストファイルの例です。\n')
     file.write('2行目のテキストです。')
 
 print('テキストファイルが生成されました。')
+"""
+
+import os
+
+# 現在のディレクトリを表示
+current_directory = os.getcwd()
+print("現在のディレクトリ:", current_directory)
