0e4e53434161a3e1201d733c0e4828b0934afe77
dimebag29 <fused.deposition.modeling@gmail.com>
2025-03-18 17:06:41 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+"""
 a = np.array([1, 2])
 b = np.array([3, 4])
 print(a + b)
+"""
+
+with open('example.txt', 'w', encoding='utf-8') as file:
+    # ファイルに書き込む内容
+    file.write('こんにちは！これはテキストファイルの例です。\n')
+    file.write('2行目のテキストです。')
+
+print('テキストファイルが生成されました。')
